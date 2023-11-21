9b02c59eff12d5d7a37e5ea065372755f9138c5e
Mau38 <maudiaz0109@gmail.com>
2023-11-20 18:02:29 -0600

    Field Centric Drive (Tune Quartenion)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -7,6 +7,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,6 +19,9 @@ public class Drivetrain {
     Map<DriveMotors, DcMotor> motors = new HashMap<DriveMotors, DcMotor>();
     Gamepad gamepad;
     HardwareMap myHardwarmap;
+
+    YawPitchRollAngles botAngles;
+
     public Drivetrain(Gamepad _gamepad, HardwareMap _myHardwarmap) {
         gamepad = _gamepad;
         myHardwarmap = _myHardwarmap;
@@ -31,30 +38,34 @@ public class Drivetrain {
     }
 
     public void drive() {
-        double x = gamepad.left_stick_x;
-        double y = -gamepad.left_stick_y;
-        double turn = -gamepad.right_stick_x;
-
-        double theta = Math.atan2(y, x);
-        double power = Math.hypot(x, y);
-
-        //double D1power = Range.clip(x + y + turn)
-
-        double sinA = Math.sin(theta - Math.PI / 4);
-        double cosA = Math.cos(theta - Math.PI / 4);
-        double max = Math.max(Math.abs(sinA), Math.abs(cosA));
-
-        motors.get(DriveMotors.D1).setPower(power * cosA/max + turn);
-        motors.get(DriveMotors.D2).setPower(power * sinA/max - turn);
-        motors.get(DriveMotors.D3).setPower(power * sinA/max + turn);
-        motors.get(DriveMotors.D4).setPower(power * cosA/max - turn);
-
-        if ((power + Math.abs(turn)) > 1) {
-            motors.forEach((name, motor) -> {
-                motor.setPower(motor.getPower() / (power + turn));
-            });
-        }
+        drive(gamepad.left_stick_x, -gamepad.left_stick_y, -gamepad1.right_stick_x);
     }
+
+    /**
+     *
+     * @param myIMU Pass in an IMU
+     * @brief Field centric driving
+     */
+
+    public void drive(IMU myIMU) {
+        myIMU.resetYaw();
+
+        botAngles = myIMU.getRobotYawPitchRollAngles();
+
+        double botYaw = botAngles.getYaw(AngleUnit.RADIANS);
+
+        double rotX = gamepad.left_stick_x * Math.cos(-botYaw) - -gamepad.left_stick_y * Math.sin(-botYaw);
+        double rotY = gamepad.left_stick_x * Math.sin(-botYaw) + -gamepad.left_stick_y * Math.cos(-botYaw);
+
+        drive(rotX, rotY, -gamepad.right_stick_x);
+    }
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @param turn
+     */
 
     public void drive(double x, double y, double turn) {
 
